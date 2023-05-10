9ba6ac6b9ede8e54be6aa6d21ebe7cac0b73289c
Saurabh9792 <saurabhshukla8813@gmail.com>
2023-05-10 22:20:30 +0530

    Commit Theme Changes

--- TechSquad.Application/Controllers/HomeController.cs
+++ TechSquad.Application/Controllers/HomeController.cs
@@ -23,11 +23,20 @@ namespace TechSquad.Application.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
